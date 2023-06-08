ca771c0c13081173ca37562e57c33b134253aea1
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2023-06-08 12:01:47 -0700

    first pass at functional

--- builder.sh
+++ builder.sh
@@ -2,8 +2,17 @@
 
 test -z "$(which platformio 2>/dev/null)" && $(which pip3) install platformio
 
-
-
 for cfg in $(ls configs/); do
-	echo ${cfg}
+	  echo Configuring ${cfg}
+    cd configs/${cfg}
+    for branch in $(ls); do
+        git clone -b ${branch} https://github.com/MarlinFirmware/Marlin current-build
+        test -e ${branch}/platformio.ini && \
+            rm -fv current-build/platformio.ini && \
+            cp -v ${branch}/platformio.ini current-build/
+        cp -v ${branch}/*.h current-build/Marlin/
+        cd current-build
+        platformio run -t build
+    done
+    cd ../../
 done
